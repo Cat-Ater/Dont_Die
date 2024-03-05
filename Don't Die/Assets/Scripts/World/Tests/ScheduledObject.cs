8d21e1ca6c9a10084a47bcf64b952a2c204e5f5e
Cat Ater <Cheshyrecat1337@gmail.com>
2024-03-05 21:37:52 +1100

    Imported Polar Lib.
    Imported Polar EQN Lib, for the purpose of generating patterns.

---
+++ Don't Die/Assets/Scripts/World/Tests/ScheduledObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public abstract class ScheduledObject : MonoBehaviour, ITimerScheduler
+{
+    public abstract float RegistrationTime { get; }
+    public abstract string RegistrationString { get; }
+    private bool NotifiedState { get; set; } = false;
+
+    internal void Start()
+    {
+        GameManager.Instance.ScheduleObject(this);
+    }
+
+    string ITimerScheduler.RegistrationString => RegistrationString;
+
+    float ITimerScheduler.RegistrationTime => RegistrationTime;
+
+    bool ITimerScheduler.Notified => NotifiedState;
+
+    void ITimerScheduler.Notification()
+    {
+        NotifiedState = true;
+        NotificationEvent(); 
+    }
+
+    internal abstract void NotificationEvent();
+}
--- Don't Die/Assets/Scripts/World/Tests/Test_TimerScheduler.cs
+++ Don't Die/Assets/Scripts/World/Tests/Test_TimerScheduler.cs
@@ -16,29 +16,3 @@ public class Test_TimerScheduler : ScheduledObject
         GameObject.Instantiate(spawnTest, spawnPosition, Quaternion.identity);
     }
 }
-
-public abstract class ScheduledObject : MonoBehaviour, ITimerScheduler
-{
-    public abstract float RegistrationTime { get; }
-    public abstract string RegistrationString { get; }
-    private bool NotifiedState { get; set; } = false;
-
-    internal void Start()
-    {
-        GameManager.Instance.ScheduleObject(this);
-    }
-
-    string ITimerScheduler.RegistrationString => RegistrationString;
-
-    float ITimerScheduler.RegistrationTime => RegistrationTime;
-
-    bool ITimerScheduler.Notified => NotifiedState;
-
-    void ITimerScheduler.Notification()
-    {
-        NotifiedState = true;
-        NotificationEvent(); 
-    }
-
-    internal abstract void NotificationEvent();
-}
