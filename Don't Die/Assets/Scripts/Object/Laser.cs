d970e3cd275588faa6264280af126efc80f5a416
Cat Ater <Cheshyrecat1337@gmail.com>
2024-03-07 14:16:13 +1100

    Refined the lasers.

--- Don't Die/Assets/Scripts/Object/Laser.cs
+++ Don't Die/Assets/Scripts/Object/Laser.cs
@@ -5,30 +5,46 @@ using UnityEngine;
 public class Laser : MonoBehaviour
 {
     private Quaternion initalRotation;
-    private Vector3 initalScale; 
-    public GameObject sprite;
+    private Vector3 initalScale;
+    [SerializeField] private float currentScale = 0F;
+    private float scaleStep;
+    private float currentTime;
+    public GameObject child;
     public Vector3 pivotOffset;
     public Vector2 centerPos;
     public Vector2 endPos;
-    public float scaleF = 0.1f;
+    public float maxScale = 0.1f;
     public float rotation = 0.1f;
+    public float extendTime = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
         initalScale = gameObject.transform.localScale;
         initalRotation = gameObject.transform.rotation;
+        scaleStep = maxScale / extendTime;
+        gameObject.transform.rotation = initalRotation * Quaternion.Euler(new Vector3(0, 0, rotation));
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.localScale = new Vector3(initalScale.x + scaleF, initalScale.y, initalScale.z);
-        gameObject.transform.rotation = initalRotation * Quaternion.Euler(new Vector3(0, 0, rotation));
+        if (currentTime < extendTime)
+        {
+            currentTime += Time.deltaTime;
+            if (currentTime >= extendTime)
+                currentTime = extendTime;
+            currentScale = currentTime * scaleStep;
+        }
+        if (currentScale > maxScale)
+            currentScale = maxScale;
+
+        gameObject.transform.localScale = new Vector3(initalScale.x + currentScale, initalScale.y, initalScale.z);
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawLine(centerPos, endPos);
+        Gizmos.color = Color.red;
     }
 }
