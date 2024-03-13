085afec398422c3d939bd93faae9329521173337
Cat Ater <Cheshyrecat1337@gmail.com>
2024-03-14 02:24:16 +1100

    Added Hidden Consumable.

--- Don't Die/Assets/Scripts/Object/Consumable/PlayerConsumablePickup.cs
+++ Don't Die/Assets/Scripts/Object/Consumable/PlayerConsumablePickup.cs
@@ -4,7 +4,7 @@ public class PlayerConsumablePickup : MonoBehaviour
 {
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "PLayer")
+        if(collision.gameObject.tag == "Player")
         {
             PlayerController.PlayerObject.GetComponent<PlayerConsumable>().consumableCount++;
             gameObject.SetActive(false);
