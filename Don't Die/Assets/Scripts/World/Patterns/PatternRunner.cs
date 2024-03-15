eb411b1c8fe5fda4df44701e2632706bec7685c0
Cat Ater <Cheshyrecat1337@gmail.com>
2024-03-16 07:42:35 +1100

    Pattern_1 Completion, Added pattern dev scene.

---
+++ Don't Die/Assets/Scripts/World/Patterns/PatternRunner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PatternRunner : MonoBehaviour
+{
+    public Pattern pattern;
+    public Pattern currentRun;
+
+    public bool repeat = false;
+
+    public void Start()
+    {
+        CreateCopy();
+        StartCoroutine(StartupDelay());
+    }
+
+    private void CreateCopy()
+    {
+        currentRun = new Pattern();
+        currentRun.name = pattern.name;
+        currentRun.order = pattern.order;
+        currentRun.patternDialogues = pattern.patternDialogues;
+        currentRun.patternEndTime = pattern.patternEndTime;
+        currentRun.patternObjects = pattern.patternObjects;
+    }
+
+    public IEnumerator StartupDelay()
+    {
+        yield return new WaitForSeconds(5.5f);
+        GameManager.GameTimer.Enabled = true;
+    }
+
+    public void Update()
+    {
+
+        pattern.Update(GameManager.GameTimer.Time);
+
+        //Check if pattern completed. 
+        if (currentRun.PatternCompletion(GameManager.GameTimer.Time) && repeat)
+        {
+            GameManager.Instance.DestroyObjects(); 
+            CreateCopy();
+            GameManager.GameTimer.ResetTimer();
+            StartCoroutine(StartupDelay());
+        }
+        else if (currentRun.PatternCompletion(GameManager.GameTimer.Time) && !repeat)
+        {
+            Debug.Log("Pattern Completed");
+        }
+    }
+}
